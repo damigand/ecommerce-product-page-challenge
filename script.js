3a6922f47010239cf665f7a09536e2758386c132
David Miguel Andrés <damigand700@gmail.com>
2024-07-24 19:44:21 +0200

    image swap done with animation and header
    animation of both images and header done to change styles on click.

---
+++ script.js
@@ -0,0 +1,37 @@
+const smallImages = document.querySelectorAll('.img-wrapper');
+const mainImage = document.querySelector('#main-image');
+
+var previousSmallImage = document.querySelector('.img-wrapper.selected');
+
+var counter = 1;
+
+for (const image of smallImages) {
+    const newClass = `active-${counter}`;
+    image.addEventListener('click', (event) => {
+        mainImage.classList.remove('active-1');
+        mainImage.classList.remove('active-2');
+        mainImage.classList.remove('active-3');
+        mainImage.classList.remove('active-4');
+        mainImage.classList.add(newClass);
+
+        previousSmallImage.classList.remove('selected');
+        image.classList.add('selected');
+
+        previousSmallImage = image;
+    });
+
+    counter++;
+}
+
+var selectedHeaderItem = document.querySelector('.header-list .selected');
+const headerItems = document.querySelectorAll('.header-list li');
+
+for (const item of headerItems) {
+    item.addEventListener('click', (event) => {
+        selectedHeaderItem.classList.remove('selected');
+
+        item.classList.add('selected');
+
+        selectedHeaderItem = item;
+    });
+}
