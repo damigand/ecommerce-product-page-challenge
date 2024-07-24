d72053576078732933cc386b96208b58af5e3435
David Miguel Andrés <damigand700@gmail.com>
2024-07-25 01:58:24 +0200

    some javascript action and cart moved outside header
    javascript action to change elements and stuff. Some more responsive stuff around the main element, and cart items moved to the main element for further responsiveness on mobile (header was limiting its width and caused it to overflow).

--- script.js
+++ script.js
@@ -1,21 +1,21 @@
-const smallImages = document.querySelectorAll('.img-wrapper');
-const mainImage = document.querySelector('#main-image');
+const smallImages = document.querySelectorAll(".img-wrapper");
+const mainImage = document.querySelector("#main-image");
 
-var previousSmallImage = document.querySelector('.img-wrapper.selected');
+var previousSmallImage = document.querySelector(".img-wrapper.selected");
 
 var counter = 1;
 
 for (const image of smallImages) {
     const newClass = `active-${counter}`;
-    image.addEventListener('click', (event) => {
-        mainImage.classList.remove('active-1');
-        mainImage.classList.remove('active-2');
-        mainImage.classList.remove('active-3');
-        mainImage.classList.remove('active-4');
+    image.addEventListener("click", (event) => {
+        mainImage.classList.remove("active-1");
+        mainImage.classList.remove("active-2");
+        mainImage.classList.remove("active-3");
+        mainImage.classList.remove("active-4");
         mainImage.classList.add(newClass);
 
-        previousSmallImage.classList.remove('selected');
-        image.classList.add('selected');
+        previousSmallImage.classList.remove("selected");
+        image.classList.add("selected");
 
         previousSmallImage = image;
     });
@@ -23,15 +23,50 @@ for (const image of smallImages) {
     counter++;
 }
 
-var selectedHeaderItem = document.querySelector('.header-list .selected');
-const headerItems = document.querySelectorAll('.header-list li');
+var selectedHeaderItem = document.querySelector(".header-list .selected");
+const headerItems = document.querySelectorAll(".header-list li");
 
 for (const item of headerItems) {
-    item.addEventListener('click', (event) => {
-        selectedHeaderItem.classList.remove('selected');
+    item.addEventListener("click", (event) => {
+        selectedHeaderItem.classList.remove("selected");
 
-        item.classList.add('selected');
+        item.classList.add("selected");
 
         selectedHeaderItem = item;
     });
 }
+
+const cart = document.querySelector(".cart");
+const cartContent = document.querySelector(".cart-content");
+
+cart.addEventListener("click", (event) => {
+    if (cartContent.style.display == "none") {
+        cartContent.style.display = "flex";
+    } else {
+        cartContent.style.display = "none";
+    }
+});
+
+document.addEventListener("click", (event) => {
+    if (event.target == cart || event.target == cart.childNodes[1]) return;
+    if (event.target != cartContent) {
+        cartContent.style.display = "none";
+    }
+});
+
+var quantity = 0;
+const quantityElement = document.querySelector(".quantity-number");
+const substract = document.querySelector(".substract");
+const add = document.querySelector(".add");
+
+add.addEventListener("click", (event) => {
+    quantity++;
+    quantityElement.innerText = quantity;
+});
+
+substract.addEventListener("click", (event) => {
+    if (quantity >= 1) {
+        quantity--;
+        quantityElement.innerText = quantity;
+    }
+});
