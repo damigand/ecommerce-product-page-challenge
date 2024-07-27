1ed0aa0f3e99e0943977a366276b5f63b379f79f
David Miguel Andrés <damigand700@gmail.com>
2024-07-27 20:45:52 +0200

    project finished
    I probably lie in the title of the commit, I'll have to check a lot of stuff still

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ const smallImages = document.querySelectorAll(".img-wrapper");
 const mainImage = document.querySelector("#main-image");
 
 var previousSmallImage = document.querySelector(".img-wrapper.selected");
+var quantity = 0;
 
 var counter = 1;
 
@@ -72,7 +73,6 @@ cart.addEventListener("click", (event) => {
     }
 });
 
-var quantity = 0;
 const quantityElement = document.querySelector(".quantity-number");
 const substract = document.querySelector(".substract");
 const add = document.querySelector(".add");
@@ -94,31 +94,11 @@ var currentImage = 1;
 const phonePrevious = document.querySelector(".previous");
 const phoneNext = document.querySelector(".next");
 
-phonePrevious.addEventListener("click", (event) => {
-    if (currentImage > 1) {
-        currentImage--;
-        const activeClass = `active-${currentImage}`;
-
-        mainImage.classList.remove("active-1");
-        mainImage.classList.remove("active-2");
-        mainImage.classList.remove("active-3");
-        mainImage.classList.remove("active-4");
-        mainImage.classList.add(activeClass);
-    }
-});
-
-phoneNext.addEventListener("click", (event) => {
-    if (currentImage < 4) {
-        currentImage++;
-        const activeClass = `active-${currentImage}`;
-
-        mainImage.classList.remove("active-1");
-        mainImage.classList.remove("active-2");
-        mainImage.classList.remove("active-3");
-        mainImage.classList.remove("active-4");
-        mainImage.classList.add(activeClass);
-    }
-});
+phonePrevious.addEventListener(
+    "click",
+    () => (currentImage = previousImage(mainImage, currentImage))
+);
+phoneNext.addEventListener("click", () => (currentImage = nextImage(mainImage, currentImage)));
 
 const menuButton = document.querySelector(".menu");
 const menuSection = document.querySelector(".menu-section");
@@ -135,12 +115,96 @@ const closeLightbox = document.querySelector(".close-lightbox");
 const lightbox = document.querySelector(".lightbox");
 
 mainImage.addEventListener("click", (event) => {
-    const width = window.innerWidth;
-    if (innerWidth > 600) {
-        lightbox.style.display = "flex";
-    }
+    lightbox.classList.toggle("active");
 });
 
 closeLightbox.addEventListener("click", (event) => {
-    lightbox.style.display = "none";
+    lightbox.classList.toggle("active");
 });
+
+var lightboxImageCounter = 1;
+
+const nextLightbox = document.querySelector(".lightbox-next");
+const previousLightbox = document.querySelector(".lightbox-previous");
+const lightboxImage = document.querySelector("#main-image-lightbox");
+
+nextLightbox.addEventListener(
+    "click",
+    () => (lightboxImageCounter = nextImage(lightboxMainImage, lightboxImageCounter))
+);
+previousLightbox.addEventListener(
+    "click",
+    () => (lightboxImageCounter = previousImage(lightboxMainImage, lightboxImageCounter))
+);
+
+function previousImage(imageElement, counter) {
+    if (counter > 1) {
+        counter--;
+        const activeClass = `active-${counter}`;
+
+        imageElement.classList.remove("active-1");
+        imageElement.classList.remove("active-2");
+        imageElement.classList.remove("active-3");
+        imageElement.classList.remove("active-4");
+        imageElement.classList.add(activeClass);
+    }
+    return counter;
+}
+
+function nextImage(imageElement, counter) {
+    if (counter < 4) {
+        counter++;
+        const activeClass = `active-${counter}`;
+
+        imageElement.classList.remove("active-1");
+        imageElement.classList.remove("active-2");
+        imageElement.classList.remove("active-3");
+        imageElement.classList.remove("active-4");
+        imageElement.classList.add(activeClass);
+    }
+    return counter;
+}
+
+const addToCartButton = document.querySelector(".add-to-cart");
+addToCartButton.addEventListener("click", () => addToCart());
+
+function addToCart() {
+    if (quantity < 1) return;
+
+    const price = 125;
+    const totalPrice = price * quantity;
+    const cartItems = document.querySelector(".cart-items");
+
+    const cartHtml = `
+        <div class="item">
+            <img
+                src="images/image-product-1-thumbnail.jpg"
+                alt="item icon"
+                class="item-image"
+            />
+            <div class="item-info">
+                <span class="item-title">Fall Limited Edition Sneakers</span>
+                <span class="item-price">
+                    $${price} x ${quantity}
+                    <span class="total-price"> $${totalPrice}.00 </span>
+                </span>
+            </div>
+            <img
+                src="images/icon-delete.svg"
+                alt="remove item icon"
+                class="delete-item"
+            />
+        </div>
+        <button class="checkout">Checkout</button>
+    `;
+
+    cartItems.innerHTML = cartHtml;
+
+    const removeItem = document.querySelector(".delete-item");
+
+    removeItem.addEventListener("click", () => {
+        cartItems.innerHTML = `
+        <p>The cart is empty</p>
+    `;
+    });
+}
